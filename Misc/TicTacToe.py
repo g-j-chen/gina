fa68cd84febedcc1c90decce545426f113ebe2e5
g-j-chen <56104832+g-j-chen@users.noreply.github.com>
2020-01-02 20:58:22 +1100

    Updating TicTacToe game with GUI

--- Misc/TicTacToe.py
+++ Misc/TicTacToe.py
@@ -1,4 +1,6 @@
-class TicTacToe:
+import tkinter as tk
+
+class TicTacToe(tk.Tk):
 	def __init__(self):
 		global board
 		self.board = [[0 for j in range(3)] for i in range(3)]
@@ -8,6 +10,51 @@ class TicTacToe:
 		self.player = 1
 		global numMoves
 		self.numMoves = 0
+		global aiPlayer
+		self.aiPlayer = False
+
+		tk.Tk.__init__(self)
+		self.title('TicTacToe game')
+		self.top_frame = tk.Frame(self, width = 409, height = 70, pady = 3)
+		self.top_frame.pack()
+		self.play_button = tk.Menubutton(self.top_frame, text = 'Play')
+		self.play_button.pack(padx = 100, pady = 30, side = tk.LEFT)
+		option = tk.Menu(self.play_button)
+		option.add_command(label = 'vs computer', command = self.setAI)
+		option.add_command(label = 'vs human', command = self.setHuman)
+		self.play_button.config(menu = option)
+		self.reset_button = tk.Button(self.top_frame, text = 'Reset', command = self.resetBoard)
+		self.reset_button.pack(padx = 100, pady = 30, side = tk.LEFT)
+		
+		self.middle_frame = tk.Frame(self)
+		self.middle_frame.pack()
+		self.button_0 = tk.Button(self.middle_frame, padx = 45, pady = 45)
+		self.button_1 = tk.Button(self.middle_frame, padx = 45, pady = 45)
+		self.button_2 = tk.Button(self.middle_frame, padx = 45, pady = 45)
+		self.button_3 = tk.Button(self.middle_frame, padx = 45, pady = 45)
+		self.button_4 = tk.Button(self.middle_frame, padx = 45, pady = 45)
+		self.button_5 = tk.Button(self.middle_frame, padx = 45, pady = 45)
+		self.button_6 = tk.Button(self.middle_frame, padx = 45, pady = 45)
+		self.button_7 = tk.Button(self.middle_frame, padx = 45, pady = 45)
+		self.button_8 = tk.Button(self.middle_frame, padx = 45, pady = 45)
+
+		self.button_0.grid(row = 0, column = 0)
+		self.button_1.grid(row = 0, column = 1)
+		self.button_2.grid(row = 0, column = 2)
+		self.button_3.grid(row = 1, column = 0)
+		self.button_4.grid(row = 1, column = 1)
+		self.button_5.grid(row = 1, column = 2)
+		self.button_6.grid(row = 2, column = 0)
+		self.button_7.grid(row = 2, column = 1)
+		self.button_8.grid(row = 2, column = 2)
+
+		self.bottom_frame = tk.Frame(self)
+		self.bottom_frame.pack()
+		self.message = tk.Label(self.bottom_frame, text = 'message', pady = 30)
+		self.message.pack()
+
+
+
 				
 	def printBoard(self):
 		print('')
@@ -25,6 +72,17 @@ class TicTacToe:
 			print(' ' + ' | '.join(row))
 		print('')
 
+
+	def resetBoard(self):
+		self.board = [[0 for j in range(3)] for i in range(3)]
+
+	def setAI(self):
+		self.aiPlayer = True
+		self.startGame()
+
+	def setHuman(self):
+		self.aiPlayer = False
+		self.startGame()
 
 	def checkColumns(self):
 		for j in range(self.length):
@@ -132,42 +190,28 @@ class TicTacToe:
 
 
 	def startGame(self):
-		answer = input(' Hello, do you want to play a game of TicTacToe? yes/no: ')
-		if answer == 'yes':
-			answer2 = input(" Great! Do you want to play against the computer? yes/no: ")
-			print(" Let's start!")
-			if answer2 == 'yes':
-				ai = True
-			else:
-				ai = False
-			while True:
-				self.printBoard()
-				if ai:
-					if self.player == 1:
-						self.makeMove()
-					else:
-						self.aiMove()
-
-				else:
+		while True:
+			self.printBoard()
+			if self.aiPlayer:
+				if self.player == 1:
 					self.makeMove()
+				else:
+					self.aiMove()
 
-				if self.checkWin() != 0:
-					self.printBoard()
-					print(' Player ' + str(1 if self.checkWin() == 1 else 2) + ' has won!')
-					break
+			else:
+				self.makeMove()
 
-				if self.numMoves == self.length ** 2:
-					self.printBoard()
-					print(" It's a draw!")
-					break
+			if self.checkWin() != 0:
+				self.printBoard()
+				print(' Player ' + str(1 if self.checkWin() == 1 else 2) + ' has won!')
+				break
 
-				self.player *= -1
+			if self.numMoves == self.length ** 2:
+				self.printBoard()
+				print(" It's a draw!")
+				break
 
-		elif answer == 'no':
-			print(' Aww... :(')
-		else:
-			print(" Sorry, I didn't understand that...")
+			self.player *= -1
 
 if __name__ == '__main__':
-	t = TicTacToe()
-	t.startGame()
+	TicTacToe().mainloop()
