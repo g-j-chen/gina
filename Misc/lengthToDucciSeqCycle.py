367aba1131a803c4d4ce3ff94b801cbb0b5df661
Gina Chen <gina.j.chen@gmail.com>
2020-05-21 15:12:12 +1000

    Pushing changes to Ducci sequence program

--- Misc/lengthToDucciSeqCycle.py
+++ Misc/lengthToDucciSeqCycle.py
@@ -1,3 +1,5 @@
+import statistics
+
 def getNextTuple(b):
     s = []
     for i in range(len(b) - 1):
@@ -11,31 +13,58 @@ def getNextTuple(b):
         s.append('1')
     return ''.join(s)
 
-def lengthToCycle(n):
+def lengthToCycle(b, p):
+    n = int(b, 2)
     if bin(n).count("1") == 2:
         print(n, "is already in a cycle - exiting");
-        return;
+        return
 
-    b = "{0:b}".format(n)
-    z = len(b)
+    print("Starting interations")
     numSeen = {n: 0}
     i = 1
+    length = 0
+    period = 0
     print(b)
     while n != 0:
-        b = getNextTuple(b.zfill(z))
+        b = getNextTuple(b.zfill(p))
         print(b)
         n = int(b, 2)
         if n in numSeen:
             start = numSeen.get(n)
             period = i - start
+            length = start
             print("period of cycle is", period)
-            print("length to start of cycle is", start)
+            print("length to start of cycle is", length)
             break
         else:
             numSeen[n] = i
         i+=1
+    return length, period
 
+def main():
+    print("Enter the maximum length of tuples desired:")
+    m = int(input())
+    lenArr = []
+    periodArr = []
+    for p in range(3, m + 1):
+        lenValues = []
+        periodValues = []
+        for n in range(1, 2 ** p):
+            b = "{0:b}".format(n).zfill(p)
+            info = lengthToCycle(b, p)
+            print(info)
+            if info != None:
+                lenValues.append(info[0])
+                periodValues.append(info[1])
+        print("        Mean     Variance")
+        print("Length:", statistics.mean(lenValues), "     ", statistics.variance(lenValues))
+        print("Period:", statistics.mean(periodValues), "     ", statistics.variance(periodValues))
+        lenArr.append(lenValues)
+        periodArr.append(periodValues)
 
-print("Enter a number:")
-n = int(input())
-lengthToCycle(n)
+    print("length array:", lenArr)
+    print("period array:", periodArr)
+
+    
+if __name__ == "__main__":
+    main()
