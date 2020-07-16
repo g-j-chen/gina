a394bc3d71d9843476494bdd38bb0d353632129f
Gina Chen <gina.j.chen@gmail.com>
2020-07-16 18:38:52 +1000

    Pushing completed solution for GetAhead Interview Practice 1 (after misinterpreting the question initially)

--- Get Ahead Winter 2020 coding problems/FlattenedIterator.java
+++ Get Ahead Winter 2020 coding problems/FlattenedIterator.java
@@ -1,58 +1,76 @@
 import java.util.*;
 import java.lang.*;
 
+/**
+* Flattened Iterator for integers
+* Example usage:
+*	//Instantiate FlattenedIterator
+*	FlattenedIterator fi =
+*			new FlattenedIterator(				
+*				[1, 2, 3],
+*				[4, 5],
+*				[6, 7, 8]);
+*
+*	while(fi.hasNext()) {
+*		System.out.print(fi.next() + " ");
+*	}
+*
+*	//Output
+*	1 4 6 2 5 7 3 8
+*	
+* Time complexity:
+*	O(m) to instantiate FlattenedIterator, where m = number of Iterators given
+*	O(1) to determine if FlattenedIterator has more elements
+*	O(n) to iterate over all the elements
+*	Total time complexity to instantiate and iterate over all the elements: O(m + n)
+*/
 public class FlattenedIterator {
-	private int len;
-	private Iterator[] iterators;
 
-	public FlattenedIterator(Iterator ... it) {
-		this.len = it.length;
-		this.iterators = it;
+	/**
+	* A queue is used to cycle through Iterators that still have elements in their iteration. 
+	*/
+	private Queue<Iterator> queue;
+
+	/**
+	* Arguments taken: variable number of Iterators, arguments must not be null (not sure if a null Iterator is possible, though).
+	* 				   input Iterators can be empty (they'll just get dropped).
+	* Assumption: The Iterators are given in the desired order to be interleaved.
+	*/
+	public FlattenedIterator(Iterator ... iterators) {
+		this.queue = new LinkedList<>();
+		for(Iterator it : iterators) {
+			if(it.hasNext()) {
+				this.queue.add(it);
+			}
+		}
 	}
 
-	public List<Integer> flatten() {
-		List<Integer> flattened = new ArrayList<>();
-		if(this.len == 0) {
-			return flattened; 
-		}
-		
-		HashSet<Integer> finished = new HashSet<>();
-
-		int pos = 0;
-		while(true) {
-			if(!finished.contains(pos)) {
-				Iterator itr = this.iterators[pos];
-				if(itr.hasNext()) {
-					int n = (Integer)itr.next();
-					flattened.add(n);
-					itr.remove();
-				} else {
-					finished.add(pos);
-				}
-			}
-			if(finished.size() == this.len) {
-				break;
-			}
-			pos = (pos + 1) % this.len;
-		}
-		return flattened; //return or print here?
+	public boolean hasNext() {
+		return this.queue.size() > 0;
 	}
 
+	public int next() {
+		Iterator it = this.queue.remove();
+		int n = (Integer)it.next();
+		if(it.hasNext()) {
+			this.queue.add(it);
+		}
+		return n;
+	}
+	
 	public static void main(String[] args) {
-		ArrayList<Integer> list1 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
-		ArrayList<Integer> list2 = new ArrayList<Integer>(Arrays.asList(6, 7, 8, 9));
-		ArrayList<Integer> list3 = new ArrayList<Integer>(Arrays.asList(10, 11, 12, 13, 14, 15));
+		ArrayList<Integer> list1 = new ArrayList<Integer>(Arrays.asList(1, 2, 3));
+		ArrayList<Integer> list2 = new ArrayList<Integer>(Arrays.asList(6, 7));
+		ArrayList<Integer> list3 = new ArrayList<Integer>(Arrays.asList(10, 11, 12));
 
 		Iterator<Integer> it1 = list1.iterator();
 		Iterator<Integer> it2 = list2.iterator();
 		Iterator<Integer> it3 = list3.iterator();
 
 		FlattenedIterator fi = new FlattenedIterator(it1, it2, it3);
-		List<Integer> combinedList = fi.flatten();
 
-		for(int i : combinedList) {
-			System.out.print(i + " ");
+		while(fi.hasNext()) {
+			System.out.print(fi.next() + " ");
 		}
 	}
-
 }
